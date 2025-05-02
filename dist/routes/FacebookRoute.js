2cea9d3a90726b3c68358196e3c4ada2ecfe8e69
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-05-02 20:57:37 +0100

    untracked node-modules and dist folder

--- dist/routes/FacebookRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const FacebookController_1 = require("../controllers/FacebookController");
-const router = (0, express_1.Router)();
-router.get("/auth/facebook", FacebookController_1.redirectToFacebook);
-router.get("/auth/facebook/callback", FacebookController_1.handleFacebookCallback);
-exports.default = router;
--- dist/routes/GoogleRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const GoogleController_1 = require("../controllers/GoogleController");
-const router = (0, express_1.Router)();
-router.get('/auth/google', GoogleController_1.redirectToGoogle);
-router.get('/auth/google/callback', GoogleController_1.handleGoogleCallback);
-router.get('/logout', GoogleController_1.logout);
-exports.default = router;
